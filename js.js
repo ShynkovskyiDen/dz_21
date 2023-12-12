fc440891ec81565fa20f0e53c43981b20e52b524
ShynkovskyiDen <shinkovskiy.dx@gmail.com>
2023-12-12 13:55:32 +0200

    added file dz_21

---
+++ js.js
@@ -0,0 +1,54 @@
+// // 1
+// let x = +prompt('Введіть число ');
+// let n = +prompt('Введіть ступінь');
+
+
+// pow(x, n);
+
+// function pow(x, n) {
+//     if (n == 1) {
+//         return x;
+//     } else {
+//         return x * pow(x, n - 1);
+//     }
+// }
+// console.log(pow(x, n));
+
+
+// 2
+
+// const array = [1, 2, 3, 4, 5, 6, 7];
+
+// function removeElement(array, item) {
+//     for (let i = 0; i <= array.length; i++) {
+//         if (i == item) {
+//             array.splice(item-1, 1);
+//         }
+//     }
+// }
+// removeElement(array, 6);
+// console.log(array);
+
+
+// 3
+// const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+// function generateKey(length, characters) {
+//     let result = '';
+//     const charactersLength = characters.length;
+
+//     for (let i = 0; i < length; i++) {
+//         const randomIndex = Math.floor(Math.random() * charactersLength);
+//         result += characters.charAt(randomIndex);
+//     }
+//     return result;
+// }
+
+// const key = generateKey(16, characters);
+// console.log(key);
+
+
+
+
+
+
